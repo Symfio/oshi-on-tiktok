d774f7ced6528024b06171096f95405d71c94f2b
MuhBayu <bnugraha00@gmail.com>
2020-05-18 01:05:56 +0700

    change caption

--- jobs/tweet.js
+++ jobs/tweet.js
@@ -9,7 +9,7 @@ const appDir = path.dirname(require.main.filename)
 const queue = new Queue('tweetQueue', process.env.REDIS_URI)
 
 queue.process(5, async function(job, done){
-    const data = job.data;
+    const data = job.data
     if(data.videoMeta.duration > 140) {
         return done(new Error('Video too long'))
     }
@@ -17,7 +17,7 @@ queue.process(5, async function(job, done){
     download(data.videoUrl, mediaPath).then(() => {
         console.log("Video Downloaded")
         const username = data.authorMeta.name
-        tweet_with_video(`Tiktok Update from  [${username}]`, mediaPath).then(async(t) => {
+        tweet_with_video(`Update from [${username}]`, mediaPath).then(async(t) => {
             await tweet(`Download disini: ${process.env.DOWNLOAD_SERVICE_URL}/${username}/${data.id}`, t.id_str)
             const result = {
                 tiktok_id: data.id,
@@ -28,7 +28,7 @@ queue.process(5, async function(job, done){
                 tiktok_createTime: data.createTime,
                 videoPath: mediaPath
             }
-            return done(null, result);
+            return done(null, result)
         }).catch(err => done(new Error(err)))
     }).catch(err => done(new Error(err)))
     
@@ -36,7 +36,7 @@ queue.process(5, async function(job, done){
 
 queue.on('progress', function(job, progress) {
     // console.log(`Job ${job.data.id} is ${progress * 100}% ready!`);
-})
+});
 
 queue.on('completed', async function(job, result){
     console.log(`${job.data.id} COMPLETED`)
@@ -46,8 +46,8 @@ queue.on('completed', async function(job, result){
             fs.unlinkSync(result.videoPath)
         }, 2000)
     } catch (error) {}
-    job.remove();
-})
+    job.remove()
+});
 queue.on('failed', function(job, err){
     console.error(err)
     const mediaPath = appDir + '/downloads/' + job.data.id + '.mp4'
@@ -57,7 +57,7 @@ queue.on('failed', function(job, err){
         }, 2000)
     } catch (error) {}
     job.remove()
-})
+});
 
 
 module.exports = queue
