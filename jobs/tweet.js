df54ae167dfcc3c785b46e87688bf59ec783079e
MuhBayu <bnugraha00@gmail.com>
2020-05-27 10:04:25 +0700

    revert

--- jobs/tweet.js
+++ jobs/tweet.js
@@ -71,10 +71,10 @@ queue.on('completed', async function(job, result){
             fs.unlinkSync(filePath)
         }
     } catch (error) {}
-    setTimeout(async() => {
-        await tweet(`Download video: ${result.downloadUrl}`, result.tweet_id)
-        job.remove()
+    setTimeout(() => {
+        tweet(`Download video: ${result.downloadUrl}`, result.tweet_id)
     }, 1000 * 60 * 2)
+    job.remove()
 });
 queue.on('failed', function(job, err){
     console.error(err)
