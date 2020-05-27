1ee9360551cb05d9e1c806c9b43f9f1901aa4bb6
MuhBayu <bnugraha00@gmail.com>
2020-05-27 09:54:53 +0700

    fix typos

--- util/download.js
+++ util/download.js
@@ -18,7 +18,7 @@ function downloadFile(videoUrl, fileName) {
 				headers: {
 					'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36'
 				}
-			}).then(r => r.a.request.uri.href)
+			}).then(r => r.request.uri.href)
 		}
 		
 		return progress(request(videoUrl), {
